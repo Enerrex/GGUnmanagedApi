ec7d90a785ecd8b7a3ba6e450fd252da28da9763
Enerrex <enerrex@gmail.com>
2023-06-04 19:55:46 -0400

    Add extension helpers for Dispoable

---
+++ GGUnmanagedCore/Containers/Iterators/Extensions/ArrayIteratorDisposables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnmanagedAPI.Iterator;
+
+namespace UnmanagedCore.Containers.Iterators.Extensions
+{
+    public static class ArrayIteratorExtensions
+    {
+        public static void DisposeAll<TUnmanaged>
+        (
+            this ArrayIterator<TUnmanaged> items
+        )
+            where TUnmanaged : unmanaged, IDisposable
+        {
+            while (items.MoveNext().Success)
+            {
+                items.Current.Dispose();
+            }
+        }
+    }
+}
---
+++ GGUnmanagedCore/Containers/Iterators/Extensions/Disposable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnmanagedAPI.Iterator;
+
+namespace UnmanagedCore.Containers.Iterators.Extensions
+{
+    public static class Disposable
+    {
+        public static void Dispose<TUnmanaged>
+        (
+            this TUnmanaged item
+        )
+            where TUnmanaged : unmanaged, IDisposable
+        {
+            item.Dispose();
+        }
+
+        public static void Dispose<TUnmanaged, TIterator>
+        (
+            this TIterator items
+        )
+            where TUnmanaged : unmanaged, IDisposable
+            where TIterator : unmanaged, IUnmanagedIterator<TUnmanaged>
+        {
+            while (items.MoveNext())
+            {
+                
+            }
+        }
+    }
+}
