6250ccc1742da9f424dc939a5d0a66702ef1511a
Enerrex <enerrex@gmail.com>
2023-06-04 21:01:29 -0400

    Move extensions

---
+++ GGUnmanagedAPI/Allocation/Extensions/Disposable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace UnmanagedAPI.Extensions
+{
+    public static class Disposable
+    {
+        public static unsafe void DisposeUnderlying<TUnmanaged>
+        (
+            this UnmanagedAPI.Allocation.Reference<TUnmanaged> item
+        )
+            where TUnmanaged : unmanaged, IDisposable
+        {
+            item.Pointer->Dispose();
+        }
+        
+        public static unsafe void DisposeUnderlying<TUnmanaged>
+        (
+            this UnmanagedAPI.Allocation.Owner<TUnmanaged> item
+        )
+            where TUnmanaged : unmanaged, IDisposable
+        {
+            item.Pointer->Dispose();
+        }
+        
+        public static unsafe void DisposeUnderlying<TUnmanaged>
+        (
+            this UnmanagedAPI.Allocation.Owner item
+        )
+            where TUnmanaged : unmanaged, IDisposable
+        {
+            item.As<TUnmanaged>()->Dispose();
+        }
+        
+        public static unsafe void DisposeWithUnderlying<TUnmanaged>
+        (
+            this UnmanagedAPI.Allocation.Owner<TUnmanaged> item
+        )
+            where TUnmanaged : unmanaged, IDisposable
+        {
+            item.Pointer->Dispose();
+            item.Dispose();
+        }
+        
+        public static unsafe void DisposeWithUnderlying<TUnmanaged>
+        (
+            this UnmanagedAPI.Allocation.Owner item
+        )
+            where TUnmanaged : unmanaged, IDisposable
+        {
+            item.As<TUnmanaged>()->Dispose();
+            item.Dispose();
+        }
+    }
+}
