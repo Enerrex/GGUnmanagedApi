a0af19d72586c3d7b85833b65a82ec08574eb2a5
Joseph Pritchett <jpritch71@gmail.com>
2023-07-27 10:29:58 -0400

    Switch

--- GGUnmanagedCore/Containers/Hashing/HashSet.cs
+++ GGUnmanagedCore/Containers/Hashing/HashSet.cs
@@ -13,12 +13,12 @@ namespace UnmanagedCore.Containers.Hashing
     {
         private TUnmanagedKeyHandler _keyHandler;
         internal float LoadFactor => Count / (float)Capacity;
-        
+
         private Allocation.Owner<HomeBucket> _storage;
 
         public int Count { get; private set; }
         public int Capacity { get; private set; }
-        
+
         internal static readonly int NeighborHoodSize = 32;
         internal static readonly int LookAheadLimit = 256;
 
@@ -34,27 +34,27 @@ namespace UnmanagedCore.Containers.Hashing
             Count = 0;
             _keyHandler = new TUnmanagedKeyHandler();
         }
-        
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal int GetBucketIndex(TUnmanagedKey key)
         {
             int hash = _keyHandler.GetHashCode(key);
             return WrapIndex(hash == int.MaxValue ? 0 : hash);
         }
-        
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal unsafe HomeBucket* GetHomeBucketPointer(TUnmanagedKey key)
         {
             var index = GetBucketIndex(key);
             return GetIndexPointer(index);
         }
-        
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal unsafe HomeBucket* GetIndexPointer(int index)
         {
             return _storage.ToPointer(index);
         }
-        
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal int WrapIndex(int index)
         {
@@ -71,12 +71,12 @@ namespace UnmanagedCore.Containers.Hashing
                 home_bucket->SetOccupied();
                 return;
             }
-            
+
             // Find next open slot
             var distance = 1;
             var current_ix = (bucket_ix + distance) % Capacity;
-            var current_bucket = GetIndexPointer(current_ix);
-            
+            HomeBucket* current_bucket = GetIndexPointer(current_ix);
+
             while (distance < LookAheadLimit)
             {
                 if (!current_bucket->IsOccupied)
@@ -89,30 +89,36 @@ namespace UnmanagedCore.Containers.Hashing
                 current_bucket = GetIndexPointer(current_ix);
             }
 
-            // Empty slot is IN the neighborhood
-            if (distance < NeighborHoodSize)
+            if (distance < LookAheadLimit)
             {
-                home_bucket->SetBit(distance);
-                current_bucket->Key = key;
-                return;
+                // Empty slot is IN the neighborhood
+                if (distance < NeighborHoodSize)
+                {
+                    home_bucket->SetBit(distance);
+                    current_bucket->Key = key;
+                    return;
+                }
             }
-            
-            // Otherwise attempt to move the empty slot back to the neighborhood
-            // We do this by displacing elements in other buckets
-        }
 
-        internal bool DisplaceSlotsInNeighborhood
-        (
-            int neighborhoodIx,
-            ref int displacementCount
-        )
-        {
-            for 
-            (
-                var neighbor_ix = WrapIndex(neighborhoodIx + NeighborHoodSize - 1);
-                
-                )
         }
+        
+        internal unsafe void FindCLoserFreeBucket(HomeBucket* bucket, ref int distance)
+        {
+            HomeBucket* target_bucket = bucket - NeighborHoodSize - 1;
+
+            while (distance < LookAheadLimit)
+            {
+                if (!current_bucket->IsOccupied)
+                {
+                    break;
+                }
+
+                distance++;
+                current_ix = (bucket->Hash + distance) % Capacity;
+                current_bucket = GetIndexPointer(current_ix);
+            }
+        }
+        
 
         public void Dispose()
         {
@@ -120,39 +126,39 @@ namespace UnmanagedCore.Containers.Hashing
             {
                 _storage[bucket_ix].Dispose();
             }
-            
+
             _storage.Dispose();
         }
-        
+
         internal struct Entry
         {
             public TUnmanagedKey Key;
             public int Hash;
         }
-        
+
         internal struct HomeBucket
         {
             public bool IsOccupied => (NeighborHood & (1 << 0)) != 0;
             public TUnmanagedKey Key;
             public int NeighborHood;
-            
+
             public void SetBit(int bitIndex)
             {
                 NeighborHood |= 1 << bitIndex;
             }
-            
+
             public bool GetBit(int bitIndex)
             {
                 return (NeighborHood & (1 << bitIndex)) != 0;
             }
-            
+
             public static readonly HomeBucket Default = new HomeBucket
             {
                 Key = default,
                 // Default neighborhood is 0
                 NeighborHood = 0
             };
-            
+
             // Use the sign bit to indicate if the bucket is occupied
             public void SetOccupied()
             {
